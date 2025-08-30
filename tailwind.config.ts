ca6f623e6d811585d78575d335ca59553fdbb76e
danniel <1037897418@qq.com>
2025-08-30 20:05:39 +0800

    新增 Tailwind CSS 配置文件，启用 JIT 引擎并定义背景渐变样式

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+    // 启用 JIT 引擎（实现 Tree Shaking）和定义扫描路
+    content: [
+        "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic":
+                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+        },
+    },
+    plugins: [],
+};
+export default config;
